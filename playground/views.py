adb63e984a8e5a0cb5fe1edeba7d09c24a77da86
rakhil <rakhil@cisco.com>
2023-08-09 11:50:40 +0530

    Added django debugging tools

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 # request -> response
 
+
+def calc():
+    a = 3
+    b = 5
+    return a
+
 def say_hello(request):
-    return HttpResponse('Hello World')
+    a = calc()
+    # b = 8
+    return render(request, 'hello.html', {'name': 'Kabali'})
